5ab64ffe9f6ac068e538d411ead64433fa2e67ec
zmeu` <tsraduu@gmail.com>
2023-09-08 10:10:20 +0100

    restructurated the js file
    As I get used of not adding a whole bunch of things happening inside the event listener,
    I have added them all to a function and passed the funciton to the event listener,
    That way is cleaner and easier to work with

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,4 +1,4 @@
-window.addEventListener("keydown", (e) => {
+function playing(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
@@ -6,12 +6,14 @@ window.addEventListener("keydown", (e) => {
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
-});
-
-const keys = document.querySelectorAll(".key");
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+}
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   this.classList.remove("playing");
 }
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playing);
