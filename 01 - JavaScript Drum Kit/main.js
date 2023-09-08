7d2fcccb3844d7d1f922b82ac41d46486052e859
zmeu` <tsraduu@gmail.com>
2023-09-08 13:13:33 +0100

    Fully replaced the deprecated keycode
    As asked in the Assignment, fully replaced the deprecated keycode with code of the key  and made the drum kit work as intended

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,6 +1,6 @@
 function playing(e) {
   const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.code}"]`);
 
   if (!audio) return;
   audio.currentTime = 0;
